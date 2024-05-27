66b0ab415ffc216e10b7188b0fe71c664edf5647
Abdelrahman <boda8099@gmail.com>
2024-05-28 01:10:52 +0300

    check

---
+++ api/README.md
@@ -0,0 +1,1 @@
+<h1> AirBnB_Clone_v3 </h1>
--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 """ comments """
 from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-# import all not none
 
-if app_views is not None:
-    from api.v1.views.index import *
+app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+
+from api.v1.views.index import *
