c42e87632232e0dd8712ea7a3b30d607603744db
jawkat <kad.joliano@gmail.com>
2024-05-27 15:36:52 +0100

    try

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,13 +1,15 @@
 #!/usr/bin/python
 import os
 from flask import Flask
+from flask_cors import CORS
 from api.v1.views import app_views
 from models import storage
 
 
-
 app = Flask(__name__)
-app.register_blueprint(app_views, url_prefix='/api/v1')
+app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
+app.register_blueprint(app_views)
+cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
