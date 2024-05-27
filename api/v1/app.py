e7046a411d0c795e88e941fef904efc0ccc386a3
jawkat <kad.joliano@gmail.com>
2024-05-27 16:18:39 +0100

    task 2

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,15 +1,14 @@
 #!/usr/bin/python
-import os
+from os import environ
 from flask import Flask
-from flask_cors import CORS
 from api.v1.views import app_views
 from models import storage
 
 
+
 app = Flask(__name__)
 app.config['JSONIFY_PRETTYPRINT_REGULAR'] = True
-app.register_blueprint(app_views)
-cors = CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+app.register_blueprint(app_views, url_prefix='/api/v1')
 
 @app.teardown_appcontext
 def teardown_appcontext(exception):
@@ -17,6 +16,11 @@ def teardown_appcontext(exception):
     storage.close()
 
 if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", "0.0.0.0")
-    port = int(os.getenv("HBNB_API_PORT", 5000))
+
+    host = environ.get('HBNB_API_HOST')
+    port = environ.get('HBNB_API_PORT')
+    if not host:
+        host = '0.0.0.0'
+    if not port:
+        port = '5000'
     app.run(host=host, port=port, threaded=True)
