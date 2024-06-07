681a52d2d55a0d9b181ddfe1765f1387582ae729
abdelrahman <boda8099@gmail.com>
2024-06-07 11:16:31 -0700

    app.py

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,18 +1,18 @@
 #!/usr/bin/python3
-'''File Doc.'''
+"""File Doc"""
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
 
 @app_views.route('/status')
 def status():
-    '''status doc method.'''
+    """status doc method."""
     return jsonify({"status": "OK"}), 200
 
 
 @app_views.route('/stats')
 def stats():
-    '''retrieves the number of each objects by type.'''
+    """retrieves the number of each objects by type."""
     classes = {"amenities": "Amenity",
                "cities": "City", "places": "Place", "reviews": "Review",
                "states": "State", "users": "User"}
